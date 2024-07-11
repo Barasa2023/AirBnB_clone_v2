0e58e682c53b10664e98a996468d44c7db5d8041
Barasa2023 <123577514+Barasa2023@users.noreply.github.com>
2024-07-11 08:28:02 +0300

    Update Server IPs

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.236.46.235', '54.221.177.5']
 
 
 def do_pack():
